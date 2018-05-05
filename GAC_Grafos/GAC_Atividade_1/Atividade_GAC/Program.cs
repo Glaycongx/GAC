178d5f268ad13baf653a71095d8a37b80b9955c5
Glaycon Gomes <37042154+Glaycongx@users.noreply.github.com>
2018-05-05 16:09:16 -0300

    Add files via upload

---
+++ GAC_Grafos/GAC_Atividade_1/Atividade_GAC/Program.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_GAC
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            int opcao = 0;
+            bool testeOpcao = true;
+
+            Console.WriteLine("\n######## GAC - Grafos ########");
+
+            int qtdVertice = 0;
+            bool testeQtdVertice = true;
+
+            Console.Write("\nDigite o número de vértices: ");
+            testeQtdVertice = int.TryParse(Console.ReadLine(), out qtdVertice);
+
+            while (!testeQtdVertice)
+            {
+                Console.WriteLine("NÚMERO INVÁLIDO. Digite a quantidade de vértices: ");
+                testeQtdVertice = int.TryParse(Console.ReadLine(), out qtdVertice);
+            }
+
+            Grafo Grafo = new Grafo(qtdVertice);
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\n######## GRAFOS ########n");
+                Console.WriteLine("[1] Inserir aresta");
+                Console.WriteLine("[2] Pesquisar aresta");
+                Console.WriteLine("[3] Excluir aresta");
+                //Console.WriteLine("[4] Inserir vértice");
+                //Console.WriteLine("[5] Excluir vértice");
+                Console.WriteLine("[6] Imprimir Grafo");
+                Console.WriteLine("[7] Lista vértices adjacentes");
+                Console.WriteLine("[8] Ordem do vértice");
+                Console.WriteLine("[9] Completo");
+                //Console.WriteLine("[9] Regular");
+                //Console.WriteLine("[9] Par");
+                //Console.WriteLine("[9] Impar");
+                Console.WriteLine("[10] Grau");
+                //Console.WriteLine("[9] Sequencia de Grau");
+                Console.WriteLine("[0] Sair\n");
+
+                Console.Write("\nDigite a opção desejada: ");
+                testeOpcao = int.TryParse(Console.ReadLine(), out opcao);
+
+                while (opcao < 0 || opcao > 15 || testeOpcao == false)
+                {
+                    Console.Write("\nOPÇÃO INVÁLIDA. Digite a opção desejada: ");
+                    testeOpcao = int.TryParse(Console.ReadLine(), out opcao);
+                }
+
+                switch (opcao)
+                {
+                    case (1):
+
+                        Console.Clear();
+                        Console.WriteLine("\n********************** INSERIR ARESTA **********************");
+
+                        int V1 = 0, V2 = 0;
+                        bool testeAresta = true;
+
+                        Console.Write("\nDigite o primeiro vértice: ");
+                        testeAresta = int.TryParse(Console.ReadLine(), out V1);
+
+                        while (!testeAresta || V1 > qtdVertice)
+                        {
+                            Console.Write("\nVALOR INVÁLIDO. Digite o primeiro vértice: ");
+                            testeAresta = int.TryParse(Console.ReadLine(), out V1);
+                        }
+
+                        testeAresta = true;
+                        Console.Write("\nDigite o segundo vértice: ");
+                        testeAresta = int.TryParse(Console.ReadLine(), out V2);
+
+                        while (!testeAresta || V2 > qtdVertice)
+                        {
+                            Console.Write("\nVALOR INVÁLIDO. Digite o segundo vértice: ");
+                            testeAresta = int.TryParse(Console.ReadLine(), out V2);
+                        }
+
+                        Grafo.InsereAresta(V1, V2);
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (2):
+
+                        Console.Clear();
+                        Console.WriteLine("\n********************** PESQUISAR ARESTA ********************");
+
+                        V1 = 0;
+                        V2 = 0;
+                        testeAresta = true;
+
+                        Console.Write("\nDigite o primeiro vértice: ");
+                        testeAresta = int.TryParse(Console.ReadLine(), out V1);
+
+                        while (!testeAresta || V1 > qtdVertice)
+                        {
+                            Console.Write("\nVALOR INVÁLIDO. Digite o primeiro vértice: ");
+                            testeAresta = int.TryParse(Console.ReadLine(), out V1);
+                        }
+
+                        testeAresta = true;
+                        Console.Write("\nDigite o segundo vértice: ");
+                        testeAresta = int.TryParse(Console.ReadLine(), out V2);
+
+                        while (!testeAresta || V2 > qtdVertice)
+                        {
+                            Console.Write("\nVALOR INVÁLIDO. Digite o segundo vértice: ");
+                            testeAresta = int.TryParse(Console.ReadLine(), out V2);
+                        }
+
+                        if (Grafo.Adjacentes(V1, V2)) Console.WriteLine("\nA aresta existe. ");
+                        else Console.WriteLine("\nA aresta não existe. ");
+
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (3):
+
+                        Console.Clear();
+                        Console.WriteLine("\n********************** EXCLUIR ARESTA **********************");
+
+                        V1 = 0;
+                        V2 = 0;
+                        testeAresta = true;
+
+                        Console.Write("\nDigite o primeiro vértice: ");
+                        testeAresta = int.TryParse(Console.ReadLine(), out V1);
+
+                        while (!testeAresta || V1 > qtdVertice)
+                        {
+                            Console.Write("\nVALOR INVÁLIDO. Digite o primeiro vértice: ");
+                            testeAresta = int.TryParse(Console.ReadLine(), out V1);
+                        }
+
+                        testeAresta = true;
+                        Console.Write("\nDigite o segundo vértice: ");
+                        testeAresta = int.TryParse(Console.ReadLine(), out V2);
+
+                        while (!testeAresta || V2 > qtdVertice)
+                        {
+                            Console.Write("\nVALOR INVÁLIDO. Digite o segundo vértice: ");
+                            testeAresta = int.TryParse(Console.ReadLine(), out V2);
+                        }
+
+                        Grafo.retiraAresta(V1, V2);
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (4):
+
+                        Console.Clear();
+                        Console.WriteLine("\n********************** INSERIR VÉRTICE *********************");
+
+                        qtdVertice++;
+                        Grafo.InsereVertice();
+
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (5):
+
+                        Console.Clear();
+                        Console.WriteLine("\n********************** EXCLUIR VÉRTICE *********************");
+
+                        int vertice;
+                        bool testeVertice = true;
+
+                        Console.Write("\nDigite o número do vértice que deseja retirar: ");
+                        testeVertice = int.TryParse(Console.ReadLine(), out vertice);
+
+                        while (!testeVertice)
+                        {
+                            Console.Write("\nVALOR INVÁLIDO. Digite o número do vértice que deseja retirar: ");
+                            testeVertice = int.TryParse(Console.ReadLine(), out vertice);
+                        }
+
+                        qtdVertice--;
+                        Grafo.RetiraVertice(vertice);
+
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (6):
+
+                        Console.Clear();
+                        Console.WriteLine("\n********************** IMPRIME GRAFO ***********************");
+
+                        Grafo.ImprimeGrafo();
+
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (7):
+
+                        Console.Clear();
+                        Console.WriteLine("\n********************** LISTA VÉRTICES ADJACENTES ***********");
+
+                        int v = 0;
+                        testeVertice = true;
+
+                        Console.Write("\nDigite o vértice para listar as adjacências: ");
+                        testeVertice = int.TryParse(Console.ReadLine(), out v);
+
+                        while (!testeVertice)
+                        {
+                            Console.Write("\nVALOR INVÁLIDO. Digite o vértice para listar as adjacências: ");
+                            testeVertice = int.TryParse(Console.ReadLine(), out v);
+                        }
+
+                        Grafo.ListaDeAdjacente(v);
+
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (8):
+
+                        Console.Clear();
+
+                        Console.WriteLine("\n");
+                        Console.Write("######## ORDEM DO VÉRTICE: ########");
+                        Console.WriteLine("\n\n");
+
+                        Grafo.Exibe_Ordem(qtdVertice);
+
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (9):
+
+                        Console.Clear();
+
+                        Console.WriteLine("\n");
+                        Console.Write("######## GRAFO COMPLETO ########");
+                        Console.WriteLine("\n\n");
+
+                        Grafo.Completo();
+
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (10):
+
+                        Console.Clear();
+
+                        Console.WriteLine("\n");
+                        Console.Write("######## GRAFO COMPLETO ########");
+                        Console.WriteLine("\n\n");
+
+                        int g = 0;
+                        testeVertice = true;
+
+                        Console.Write("\nDigite o vértice para listar as adjacências: ");
+                        testeVertice = int.TryParse(Console.ReadLine(), out g);
+
+                        while (!testeVertice)
+                        {
+                            Console.Write("\nVALOR INVÁLIDO. Digite o vértice para listar as adjacências: ");
+                            testeVertice = int.TryParse(Console.ReadLine(), out g);
+                        }
+
+                        Grafo.Grau(g);
+
+                        Console.ReadKey(true);
+
+                        break;
+
+                    case (0):
+
+                        break;
+
+                    default:
+
+                        Console.Write("\nOPÇÃO INVÁLIDA!");
+                        break;
+                }
+
+            } while (opcao != 0);
+
+            Console.Write("\nPressione qualquer tecla para sair...");
+            Console.ReadKey(true);
+        }
+    }
+}
+
---
+++ GAC_Grafos/GAC_Atividade_2/Exercicio_GAC/Grafos.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_GAC
+{
+    class Grafos
+    {
+        int qtd_aresta = 0;
+
+        private int qtdVertices;
+        private Dictionary<int, List<int>> Vertice = new Dictionary<int, List<int>>();
+
+        public int QtdVertices { get => qtdVertices; set => qtdVertices = value; }
+
+        public Grafos(int qtdVertices)
+        {
+            this.QtdVertices = qtdVertices;
+
+            for (int i = 1; i < (qtdVertices + 1); i++)
+            {
+                Vertice.Add(i, new List<int>());
+            }
+        }
+
+        // Imprime o grafo (Vértice e arestas)
+        public void ImprimeGrafo()
+        {
+            foreach (KeyValuePair<int, List<int>> i in Vertice)
+            {
+                Console.Write("\nVértice: " + i.Key + "\tArestas: ");
+
+                foreach (int j in (i.Value as List<int>))
+                {
+                    Console.Write(j + "\t");
+                }
+            }
+        }
+
+        // Insere uma aresta no grafo. O procedimento recebe os vértices(v1, v2).
+        public void InsereAresta(int V1, int V2)
+        {
+            string pesquisa = "\nUm dos vértices informados não exite. ";
+
+            foreach (KeyValuePair<int, List<int>> i in Vertice)
+            {
+                if (Vertice.ContainsKey(V1) && Vertice.ContainsKey(V2))
+                {
+                    pesquisa = "";
+
+                    if (V1.Equals(i.Key))
+                    {
+                        if (i.Value.Contains(V2))
+                        {
+                            Console.Write("\nEssa aresta já foi incluída. ");
+                        }
+                        else
+                        {
+                            i.Value.Add(V2);
+                            Console.Write("\nAresta inserida com sucesso. ");
+                        }
+                    }
+                }
+            }
+            Console.Write(pesquisa);
+        }
+
+        // Verifica se existe uma determinada aresta. A função retorna true se a aresta (v1, v2) está presente no grafo, senão retorna false.
+        public bool Adjacentes(int V1, int V2)
+        {
+            bool pesquisa = false;
+
+            foreach (KeyValuePair<int, List<int>> i in Vertice)
+            {
+                if (Vertice.ContainsKey(V1))
+                {
+                    foreach (int j in (i.Value as List<int>))
+                    {
+                        if (j.Equals(V2))
+                        {
+                            pesquisa = true;
+                        }
+                    }
+                }
+            }
+            return pesquisa;
+        }
+
+        // Retira uma aresta do grafo. O procedimento retira a aresta(v1, v2) do grafo.
+        public void retiraAresta(int V1, int V2)
+        {
+            bool pesquisa = false;
+
+            if (Adjacentes(V1, V2))
+            {
+                foreach (KeyValuePair<int, List<int>> i in Vertice)
+                {
+                    if (V1.Equals(i.Key))
+                    {
+                        foreach (int j in (i.Value as List<int>))
+                        {
+                            if (j.Equals(V2))
+                            {
+                                pesquisa = true;
+                            }
+                        }
+                    }
+
+                    if (pesquisa)
+                    {
+                        (i.Value as List<int>).Remove(V2);
+                        Console.Write("\nAresta removida com sucesso. ");
+                    }
+                }
+            }
+        }
+
+        // Insere o vértice v no grafo.
+        public void InsereVertice()
+        {
+            int aux = Vertice.Keys.Max();
+            Vertice.Add((aux + 1), new List<int>());
+            Console.Write("\nVértice " + (aux + 1) + " inserido com sucesso. ");
+        }
+
+        // Retira o vértice v do grafo.
+        public void RetiraVertice(int v)
+        {
+            for (int i = 0; i < Vertice.Count; i++)
+            {
+                retiraAresta(i, v);
+            }
+
+            Vertice.Remove(v);
+
+            Console.Write("\nVértice " + v + " removido com sucesso. ");
+        }
+
+        // Obtém a lista de vértices adjacentes ao vértice v
+        public void ListaDeAdjacente(int chave)
+        {
+
+
+            string pesquisa = "Não encontrado.";
+
+            foreach (KeyValuePair<int, List<int>> i in Vertice)
+            {
+                if (chave.Equals(i.Key))
+                {
+                    pesquisa = "";
+
+                    Console.Write("\nVértice: " + i.Key + "\t");
+
+                    Console.WriteLine();
+
+                    foreach (int j in (i.Value as List<int>))
+                    {
+                        Console.Write("\n " + i.Key + "-" + j + "\t");
+                        qtd_aresta = i.Key + j;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(pesquisa);
+
+        }
+
+        public bool par(int v)
+        {
+            if ((v) % 2 == 0 && (v) != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool impar(int v)
+        {
+            if ((v) % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        //Ordem do vértice
+
+        public void Exibe_Ordem(int qtd_vertice)
+        {
+            Console.WriteLine("A ordem do vértice é: " + qtdVertices);
+        }
+
+        public void Completo()
+        {
+
+            if (qtd_aresta < (qtdVertices * (qtdVertices - 1) / 2))
+            {
+                Console.Write("O grafo não é completo! \n");
+            }
+            else
+            {
+                if (qtd_aresta < qtdVertices * (qtdVertices - 1) / 2)
+                {
+                    Console.WriteLine("O grafo é completo! \n\n");
+                }
+            }
+
+        }
+
+        public bool Regular()
+        {
+            List<int> sequenciaG = new List<int>();
+
+            foreach (KeyValuePair<int, List<int>> i in Vertice)
+            {
+                sequenciaG.Add((i.Key));
+            }
+
+            int grau = sequenciaG[0];
+
+            foreach (int i in sequenciaG)
+            {
+                if (grau != i) ;
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Grau(int chave)
+        {
+            string pesquisa = "Não encontrado.";
+
+            int count = 0;
+
+            foreach (KeyValuePair<int, List<int>> i in Vertice)
+            {
+                if (chave.Equals(i.Key))
+                {
+                    pesquisa = "";
+
+                    Console.Write("\nVértice: " + i.Key + "\t");
+
+                    Console.WriteLine();
+
+                    foreach (int j in (i.Value as List<int>))
+                    {
+                        Console.Write("\n " + i.Key + "-" + j + "\t\n");
+                        count = i.Key + j;
+
+                    }
+                }
+            }
+
+            Console.WriteLine("\n O Grau do vértice é: " + count);
+        }
+
+        public void sequenciaG()
+        {
+            List<int> sequenciaGrau = new List<int>();
+            foreach (KeyValuePair<int, List<int>> i in Vertice)
+            {
+                sequenciaGrau.Add((i.Key));
+                sequenciaGrau.Sort();
+            }
+
+            foreach (int grau in sequenciaGrau)
+            {
+                Console.WriteLine(" " + grau);
+            }
+        }
+
+        //public void ShowMA(int[,] matrizAdjacencia)
+        //{
+        //    foreach (KeyValuePair<int, List<int>> i in grafo)
+        //    {
+        //        foreach (int a in (List<int>)i.Value)
+        //            matrizAdjacencia[i.Key, a] = 1;
+        //    }
+
+        // 
+        //    Console.Write(" ");
+        //    for (int i = 0; i < matrizAdjacencia.GetLength(0); i++)
+        //    {
+        //        
+        //        Console.Write(" " + i);
+        //    }
+        //    Console.WriteLine();
+
+        //    for (int i = 0; i < matrizAdjacencia.GetLength(0); i++)
+        //    {
+        //        
+        //        Console.Write(i);
+        //        for (int j = 0; j < matrizAdjacencia.GetLength(1); j++)
+        //        {
+        //            
+        //            Console.Write(" " + matrizAdjacencia[i, j]);
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+    }
+}
